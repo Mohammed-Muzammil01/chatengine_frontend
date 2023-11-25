f7ed13e1a96ec35eb9c7f930d79889775f547925
humayunrasheed <104017282+humayunrasheed@users.noreply.github.com>
2023-11-25 18:23:05 +0530

    Header Added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ function App() {
   return (
     <div className="App">
       <div className='d-flex justify-content-center align-items-center'>
-        <ul>
-          <li>Hello1</li>
-          <li>Hello1</li>
-          <li>Hello1</li>
-          <li>Hello1</li>
-        </ul>
+        <section>
+          <div className='header'>
+            <div className='logo'>
+              logo
+            </div>
+            <div className='name text-green'>
+              website name
+            </div>
+            <div className='signUp btn border-green'>
+              <a href='#'>Sign Up / Log In</a>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
