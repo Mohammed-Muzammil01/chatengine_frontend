106eb545615b4698b31355e77362768f584084aa
Muzammil <mdmuzammil0701@gmail.com>
2023-11-26 21:00:34 +0530

    added navmenu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from '../assets/CElogo1.png'
 import { Link } from 'react-router-dom'
 
-
 function Navbar() {
+  
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
-        <div className="App">
-          <section className='header'>
-          <div className='name'>
-              website name
-            </div>
-            <div className='logo'>
-              <img style={{ height:"60px", paddingTop:"10px"}} src={Logo} alt='logo'/>
-            </div>
-            <div className='signUp'>
-              <Link href='/'>Sign Up / Log In</Link>
-            </div>
-          </section>
+      <div className="App">
+        <section className='header'>
+
+          <nav className='d-flex flex-row justify-content-center align-items-center'>
+            {/* <button className='hamburger' onClick={setMenuOpen(!isMenuOpen)}>☰</button> */}
+          <ul className='nav-items d-flex flex-row align-items-center justify-content-center mb-0'>
+            <li>
+              <Link to="/">Create</Link>
+            </li>
+            <li>
+              <Link to="/">Features</Link>
+            </li>
+            <li>
+              <Link to="/">Contact</Link>
+            </li>
+            <li id='login-link'>
+              <Link to="/">Sign-up / Login</Link>
+            </li>
+          </ul>
+          </nav>
+
+
+          <div className='logo'>
+            <img style={{ height: "60px", paddingTop: "10px" }} src={Logo} alt='logo' />
+          </div>
+          <div className='signUp'>
+            <Link href='/'>Sign Up / Log In</Link>
+          </div>
+        </section>
       </div>
     </>
   )
