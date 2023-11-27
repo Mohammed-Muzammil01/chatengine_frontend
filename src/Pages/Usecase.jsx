75802e8429120a8c09392b144ad377d9cb370dec
Hanzalashaik <shaikhhanzala27@gmail.com>
2023-11-27 15:10:33 +0000

    modified page-2

--- src/Pages/Usecase.jsx
+++ src/Pages/Usecase.jsx
@@ -1,5 +1,8 @@
 import Navbar from "../Components/Navbar"
 import { FaBriefcase } from "react-icons/fa";
+import { RiCustomerService2Line } from "react-icons/ri";
+import { IoFastFoodOutline } from "react-icons/io5";
+import { BsFillPatchQuestionFill } from "react-icons/bs";
 
 export default function UseCase() {
     return (
@@ -8,21 +11,28 @@ export default function UseCase() {
             <p className="usecase-heading">Choose your usecase:</p>
             <div className="container">
                 <div className="cards">
-                    <div className="caqrd">
+                    <div className="card">
                     <FaBriefcase className="case"/>
                     <h3>Portfolio Bot</h3>
+                        <h5>Create a custom bot trained on your data.</h5>
                     </div>
                     <div className="card">
-                        <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <RiCustomerService2Line className="case"/>
+                    <h3>Customer Service Bot</h3>
+                    <h5>Create a AI-Driven Chatbot for customer care inquires</h5>
                     </div>
                 </div>
 
                 <div className="cards">
                     <div className="card">
-                        <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <IoFastFoodOutline className="case"/>
+                    <h3>Restaurant Bot</h3>
+                    <h5>Create a talking menu for your restaurant</h5>
                     </div>
                     <div className="card">
-                        <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <BsFillPatchQuestionFill className="case"/>
+                        <h3>Others</h3>
+                        <h5>Create your own usecase</h5>
                     </div>
                 </div>
             </div>
