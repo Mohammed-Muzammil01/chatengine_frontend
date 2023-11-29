4749afb35bfa73756ec947f734f845ac8f0028ac
Muzammil <mdmuzammil0701@gmail.com>
2023-11-29 17:17:54 +0530

    fixed css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import LandingPage from './Pages/LandingPage.jsx';
 import { Routes, Route } from 'react-router-dom';
 import UseCase from './Pages/Usecase.jsx';
+import Chatbot from './Pages/Chatbot.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<LandingPage />} />
       <Route path='/usecase' element={<UseCase/>} />
-      
+      <Route path='/chatbot/:id' element={<Chatbot/>} />      
     </Routes>
   );
 }
