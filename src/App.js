321d1e98efec7655fe06b2da01d82670181c9e5d
Muzammil <mdmuzammil0701@gmail.com>
2023-11-30 14:49:50 +0530

    Added route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<LandingPage />} />
       <Route path='/usecase' element={<UseCase/>} />
+      {/* <Route path='/yourbots' element={<You/>} /> */}
       <Route path='/chatbot/:id' element={<Chatbot/>} />      
     </Routes>
   );
